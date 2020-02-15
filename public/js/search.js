c68e7278256def34aa8dbb448e8cca81edc742fb
inbary-s <inbarsim6@gmail.com>
2020-02-15 13:47:29 -0700

    books api and server update

--- public/js/search.js
+++ public/js/search.js
@@ -1,2 +1,19 @@
 // on click function
+$('#search').on('click', function(event){
+    event.preventDefault();
+    console.log('hello');
+    let searchTerm = ($('#searchForm').val().trim().toLowerCase().replace(/\s+/g, '-'))
+    $.ajax({
+        url: `http://openlibrary.org/search.json?q=${searchTerm}`,
+        method: "GET"
+      }).then(function(response) {
+        let isbn = (response.docs[0].isbn[0])
+        $.ajax({
+            url: `http://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`,
+            method: "GET"
+          }).then(function(response) {
+            console.log(response);
+          });
+      });
+})
 //ajax call
---
+++ routes/html-routes.js
@@ -0,0 +1,7 @@
+var path = require("path");
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/html/index.html"));
+    });
+}
