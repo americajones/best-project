9518134215c863a2ee555e4350d9dd11ae4d431c
AJ <lastbreath.jones@gmail.com>
2020-02-15 11:14:54 -0700

    okay now lint rules

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
-
+ 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
