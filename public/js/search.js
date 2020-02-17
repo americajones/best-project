33c9038f7e58ab4b0afe893d1fe43989468f81f8
LindsayMF <meneslindsay@hotmail.com>
2020-02-17 16:22:08 -0700

    added api call for movie poster images

--- public/js/search.js
+++ public/js/search.js
@@ -1,8 +1,8 @@
 // on click function
-$('#search').on('click', function(event){
+$("#search").on("click", function(event){
     event.preventDefault();
-    console.log('hello');
-    let searchTerm = ($('#searchForm').val().trim().toLowerCase().replace(/\s+/g, '-'))
+    console.log("hello");
+    let searchTerm = ($("#searchForm").val().trim().toLowerCase().replace(/\s+/g, '-'))
     $.ajax({
         url: `http://openlibrary.org/search.json?q=${searchTerm}`,
         method: "GET"
@@ -15,5 +15,27 @@ $('#search').on('click', function(event){
             console.log(response);
           });
       });
-})
+});
+
+
 //ajax call
+//movie api call 
+$("#search").on("click", function(event) {
+  event.preventDefault();
+  console.log("Working?");
+  let searchTerm = ($("#searchForm").val().trim().toLowerCase().replace(/\s+/g, '-'))
+  
+  $.ajax({
+    url: `https://www.omdbapi.com/?t=${searchTerm}&apikey=b63370de`,
+    method: "GET"
+  }).then(function(response) {
+    //  let movieDiv = $("<div class='movie'>");
+    //  let imgURL = response.Poster;
+
+    //  let image = $("<img>").attr("src", imgURL);
+
+    //  movieDiv.append(image);
+
+    console.log(response.Poster);
+  });
+});
