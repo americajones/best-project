6ccbc716ae519321b3c486ffc2d52d3fa6e5b4e5
AJ <lastbreath.jones@gmail.com>
2020-02-18 14:47:26 -0700

    added basic pug and css files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,20 @@
 var path = require("path");
 
 module.exports = function(app) {
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/html/index.html"));
+    // app.get("/", function(req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/html/index.html"));
+    // });
+
+    app.get("/", (req, res) => {
+        res.render("index")
     });
+    
+    app.get("/submit", (req, res) => {
+        res.render("submit")
+    });
+
+    app.get("/reviews", (req, res) => {
+        res.render("reviews")
+    });
+
 }
