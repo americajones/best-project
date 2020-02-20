558b8fc2fb62ee5912e43b8f67cb31cea7ca0221
AJ <lastbreath.jones@gmail.com>
2020-02-19 20:58:22 -0700

    added login page html

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   // Getting references to our form and inputs
-  var loginForm = $("form.login");
+  var loginForm = $("#login_user");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+  loginForm.on("click", function(event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -25,4 +25,8 @@ module.exports = function(app) {
     app.get("/signup", (req, res) => {
         res.render("signup")
     });
+
+    app.get("/login", (req, res) => {
+        res.render("login")
+    });
 }
